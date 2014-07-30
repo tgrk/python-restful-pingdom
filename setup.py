241037c19ebca16344379e95ff7a58734e7dbf7f
Martin Wiso <martin@wiso.cz>
2014-07-30 17:10:39 +0200

    Debugging pip setup script

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
 from setuptools import setup, find_packages
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
     name = "pingdom",
